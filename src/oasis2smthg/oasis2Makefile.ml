192ed3ce0ae5499c44e84aff4b5f91a04db463a8
Raphaël Proust <raphlalou@gmail.com>
2012-12-05 15:30:54 +0000

    oasis2Makefile: PROTOTYPE
    Warning, this commit is not clean. The code style is extremely bad. The
    content is also limited.
    
    oasis2Makefile translates a _oasis file into an old-school Makefile.

---
+++ src/oasis2smthg/oasis2Makefile.ml
@@ -0,0 +1,105 @@
+
+let pf = Printf.printf
+let idstr (x:string) = x
+
+let fn = Sys.argv.(1)
+
+let o = OASISAst.to_package
+	{OASISRecDescParser.
+		oasisfn = (Some fn);
+		ctxt = !OASISContext.default;
+	}
+	(Stream.of_channel (open_in fn))
+
+let section s = let open OASISTypes in
+	match s with
+	| Library (cs, bs, lib) -> assert false
+	| Object (cs, bs, o) -> assert false
+	| Executable (cs, bs, e) -> begin
+		let path = bs.bs_path in
+		let compiled_object = bs.bs_compiled_object in
+		let internal_depends, findlib_depends =
+			List.fold_left
+				(fun (id, fd) d -> match d with
+					| FindlibPackage (f, _) -> (id, f :: fd)
+					| InternalLibrary n -> (n :: id, fd)
+				)
+				([], [])
+				bs.bs_build_depends
+		in
+		let internal_tool, external_tool =
+			List.fold_left
+				(fun (it, et) t -> match t with
+					| ExternalTool n -> (it, n :: et)
+					| InternalExecutable n -> (n :: it, et)
+				)
+				([], [])
+				bs.bs_build_tools
+		in
+		let byteopt = OASISExpr.choose idstr bs.bs_byteopt in
+		let nativeopt = OASISExpr.choose idstr bs.bs_nativeopt in
+
+		let builder = match compiled_object with
+		| Byte -> "ocamlc"
+		| Native | Best -> "ocamlopt"
+		in
+		let args =
+			(match compiled_object with
+				| Byte -> String.concat " " byteopt
+				| Native | Best -> String.concat " " nativeopt
+			) ::
+			[] (*TODO: includes and such*)
+		in
+		let arg = Printf.sprintf "%s/%s" path e.exec_main_is in
+		let rule = Printf.sprintf "%s %s %s"
+			builder (String.concat " " args) arg in
+
+		(cs.cs_name, internal_depends, [rule])
+	end
+	| Flag (cs, f) -> assert false
+	| SrcRepo (cs, sr) -> assert false
+	| Test (cs, t) -> assert false
+	| Doc (cs, d) -> assert false
+
+let package2makefile
+	{ OASISTypes.
+		name;             (*package_name*)
+		version;          (*OASISVersion.t*)
+
+		conf_type;        (*[`Configure] plugin*)
+		conf_custom;      (*custom*)
+
+		build_type;       (*[`Build] plugin*)
+		build_custom;     (*custom*)
+
+		install_type;     (*[`Install] plugin*)
+		install_custom;   (*custom*)
+		uninstall_custom; (*custom*)
+
+		clean_custom;     (*custom*)
+		distclean_custom; (*custom*)
+
+		files_ab;         (*unix_filename list*)
+		sections;         (*section list*)
+		plugins;          (*[`Extra] plugin list*)
+		schema_data;      (*PropList.Data.t*)
+		plugin_data;      (*plugin_data*)
+	} =
+	let variables = [
+		("NAME", name);
+		("VERSION", OASISVersion.string_of_version version);
+	]
+	in
+	let sections = List.map section sections in
+	List.iter (fun (s, v) -> pf "%s=%s\n" s v)  variables ;
+	pf "\n";
+	List.iter
+		(fun (prereq, depends, cmds) ->
+			pf "%s: %s\n\t%s\n" prereq
+				(String.concat " " depends)
+				(String.concat "\n\t" cmds)
+		)
+		sections
+
+
+let () = package2makefile o
